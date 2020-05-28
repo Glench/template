edac4fae12af2396ec5dc391d17c58b81af204ba
Glen Chiacchieri <www@glench.com>
2020-05-28 13:06:39 -0400

    add template for main.js that allows pasting in browser

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-	}
+    target: document.body,
+    props: {
+    }
 });
+
+// // For pasting in web browsers on other pages
+// var div = document.querySelector('#svelte-app');
+// if (!div) {
+//     div = document.createElement('div');
+//     div.setAttribute('id', 'svelte-app');
+// }
+// const app = new App({
+//     target: div,
+//     props: {
+//     }
+// });
 
 export default app;
